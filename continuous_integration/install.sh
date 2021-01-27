49ab7648267bbc004b464d37b763787f765eda10
HirniMeshram1 <77767969+HirniMeshram1@users.noreply.github.com>
2021-01-27 13:54:55 +0530

    Update install.sh

--- continuous_integration/install.sh
+++ continuous_integration/install.sh
@@ -10,9 +10,8 @@ set -e
 
 # use Anaconda to get compiled versions of scipy and numpy,
 # modified from https://gist.github.com/dan-blanchard/7045057
-if [[ $TRAVIS_OS_NAME == 'linux' ]]; then wget https://github.com/conda-forge/miniforge/releases/download/4.8.2-1/Miniforge3-4.8.2-1-Linux-aarch64.sh -O miniconda.sh; fi
-if [[ $TRAVIS_OS_NAME == 'osx' ]]; then wget https://repo.continuum.io/miniconda/Miniconda3-4.2.12-MacOSX-x86_64.sh -O miniconda.sh; fi
-chmod +x miniconda.sh
+if [[ `uname -m` == 'aarch64' ]]; then wget https://github.com/conda-forge/miniforge/releases/download/4.8.2-1/Miniforge3-4.8.2-1-Linux-aarch64.sh -O miniconda.sh; fi
+if [[ `uname -m` == 'x86_64' ]]; then wget https://repo.continuum.io/miniconda/Miniconda3-4.2.12-Linux-x86_64.sh -O miniconda.sh; fi
 ./miniconda.sh -b -p $HOME/miniconda3
 export PATH=$HOME/miniconda3/bin:$PATH
 # Update conda itself
